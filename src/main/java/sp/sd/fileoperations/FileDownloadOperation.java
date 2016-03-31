2958f69da2052a1421dbb2579f28133aef02a236
Suresh Kumar P <pskumar448@gmail.com>
2016-03-31 23:25:24 +0530

    Added FileDownloadOperation

---
+++ src/main/java/sp/sd/fileoperations/FileDownloadOperation.java
@@ -0,0 +1,171 @@
+package sp.sd.fileoperations;
+
+import hudson.Launcher;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import org.kohsuke.stapler.DataBoundConstructor;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.OutputStream;
+import hudson.FilePath.FileCallable;
+import hudson.remoting.VirtualChannel;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.jenkinsci.remoting.RoleChecker;
+import java.io.Serializable;
+import java.net.URI;
+
+
+public class FileDownloadOperation extends FileOperation implements Serializable { 
+	private final String url;
+	private final String userName;
+	private final String targetLocation;
+	private final String targetFileName;
+	private final String password;
+	@DataBoundConstructor 
+	 public FileDownloadOperation(String url, String userName, String password, String targetLocation, String targetFileName) { 
+		this.url = url;
+		this.userName = userName;
+		this.targetLocation = targetLocation;
+		this.targetFileName = targetFileName;
+		this.password = password;
+	 }
+
+	 public String getUrl()
+	 {
+		 return url;
+	 }
+	 public String getUserName()
+	 {
+		 return userName;
+	 }
+	 public String getTargetLocation()
+	 {
+		 return targetLocation;
+	 }
+	 public String getTargetFileName()
+	 {
+		 return targetFileName;
+	 }
+	 public String getPassword()
+	 {
+		 return password;
+	 }
+	 public boolean RunOperation(AbstractBuild build, Launcher launcher, BuildListener listener) {
+		 boolean result = false;
+		 try
+			{
+			 listener.getLogger().println("File Download Operation:");
+				FilePath ws = build.getWorkspace(); 				
+				
+				try {	
+					result = ws.act(new TargetFileCallable(listener, build.getEnvironment(listener).expand(url), build.getEnvironment(listener).expand(userName), build.getEnvironment(listener).expand(password), build.getEnvironment(listener).expand(targetLocation), build.getEnvironment(listener).expand(targetFileName)));				
+				}
+				catch (Exception e) {
+					listener.fatalError(e.getMessage());
+					return false;
+				}
+				
+			}
+			catch(Exception e)
+			{
+				listener.fatalError(e.getMessage());
+			}	
+			return result;
+		} 
+ 
+	private static final class TargetFileCallable implements FileCallable<Boolean> {
+		private static final long serialVersionUID = 1;
+		private final BuildListener listener;
+		private final String resolvedUrl;
+		private final String resolvedUserName;
+		private final String resolvedTargetLocation;
+		private final String resolvedTargetFileName;
+		private final String resolvedPassword;
+		public TargetFileCallable(BuildListener Listener, String ResolvedUrl, String ResolvedUserName, String ResolvedPassword, String ResolvedTargetLocation, String ResolvedTargetFileName) {
+			this.listener = Listener;
+			this.resolvedUrl = ResolvedUrl;	
+			this.resolvedUserName = ResolvedUserName;
+			this.resolvedTargetLocation = ResolvedTargetLocation;
+			this.resolvedTargetFileName = ResolvedTargetFileName;
+			this.resolvedPassword = ResolvedPassword;
+		}
+		@Override public Boolean invoke(File ws, VirtualChannel channel) {
+			boolean result = false;
+			try {
+				FilePath fpWS = new FilePath(ws);
+				FilePath fpTL = new FilePath(fpWS, resolvedTargetLocation);
+				FilePath fpTLF = new FilePath(fpTL,resolvedTargetFileName);
+				File fTarget = new File(fpTLF.toURI());
+				URI Url = new URI(resolvedUrl);
+				listener.getLogger().println("Started downloading file from " + resolvedUrl);
+				HttpHost host = new HttpHost(Url.getHost(), Url.getPort(), Url.getScheme());
+				CredentialsProvider credsProvider = new BasicCredentialsProvider();
+				credsProvider.setCredentials(new AuthScope(Url.getHost(), Url.getPort()), new UsernamePasswordCredentials(resolvedUserName, resolvedPassword));				
+				AuthCache authCache = new BasicAuthCache();				
+				BasicScheme basicAuth = new BasicScheme();
+				authCache.put(host, basicAuth);
+				CloseableHttpClient httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).setRedirectStrategy(new LaxRedirectStrategy()).build();
+				HttpGet httpGet = new HttpGet(Url);				
+				HttpClientContext localContext = HttpClientContext.create();
+				localContext.setAuthCache(authCache);
+				HttpResponse response = httpClient.execute(host, httpGet, localContext);
+				HttpEntity entity = response.getEntity();
+				OutputStream fosTarget = new FileOutputStream(fTarget);
+	            try {
+	            	if(response.getStatusLine().getStatusCode()!=HttpStatus.SC_OK)
+	                {
+	            		result = false;
+	                }
+	            	else
+	            	{
+	            		entity. writeTo(fosTarget);
+		            	result = true;
+		            	listener.getLogger().println("Completed downloading file.");
+	            	}
+	            }
+	            finally 
+	            {
+	            	fosTarget.close();
+	            }
+			}
+			catch(RuntimeException e)
+			{
+				listener.fatalError(e.getMessage());
+				throw e;
+			}
+			catch(Exception e)
+			{
+				listener.fatalError(e.getMessage());
+				result = false;
+			}
+			return result;	
+		}		
+				
+		@Override  public void checkRoles(RoleChecker checker) throws SecurityException {
+                
+		}		
+	}
+	
+ @Extension public static class DescriptorImpl extends FileOperationDescriptor {
+ public String getDisplayName() { return "File Download"; }
+
+ }
+
+}
