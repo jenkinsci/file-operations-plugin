4f7702147be2e06e1798168cec9c27177cd8db61
René Scheibe <rene.scheibe@gmail.com>
2018-10-28 22:07:41 +0100

    Remove unused imports

--- src/test/java/sp/sd/fileoperations/FileDeleteOperationTest.java
+++ src/test/java/sp/sd/fileoperations/FileDeleteOperationTest.java
@@ -2,7 +2,6 @@ package sp.sd.fileoperations;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import hudson.EnvVars;
 import hudson.model.FreeStyleBuild;
